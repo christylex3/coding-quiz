9e532af51ef86bb25388e8cea2f6c8fcb82c98d7
Christy Le <christylex3@gmail.com>
2022-06-27 00:22:14 -0400

    Added 'page' class to 'start-page', 'question-page', and 'result-page'. Fixed 'question-page' to be left-aligned and flex 'answer-choices' (also new class added to buttons on 'question-page')

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var currentPage = document.querySelector(".show");
+
+function nextPage () {
+    currentPage 
+    // Clicking "Start Quiz" will change start-page to question-page
+    // Clicking any answer will move to next question-page
+    // Clicking last question-page will move to result-page
+    // If timer is over before reaching the end of the question, move to result-page
+}
