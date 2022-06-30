b92079f2771df05e74eb2a353ab09e6e8bdb303b
Christy Le <christylex3@gmail.com>
2022-06-29 21:32:12 -0400

    Cleaned up highscore-script.js file by organizing the methods and variables to be more readable

--- assets/js/highscore-script.js
+++ assets/js/highscore-script.js
@@ -1,16 +1,39 @@
 var highscoreList = document.querySelector("#highscore-list");
+var clearHighscore = document.querySelector("#clear-highscore");
 
 if (highscoreList) {
     highscoreList.addEventListener("load", displayHighscores());
 }
 
+if (clearHighscore) {
+    clearHighscore.addEventListener("click", clearList);
+}
+
+// Displays the table of highscores
 function displayHighscores() {
 
-    console.log("highscore here");
+    // Grabs the existing players' initials and score
+    var playerTrack = JSON.parse(localStorage.getItem("players"));
 
+    if (playerTrack === null) {
+        playerTrack = [];
+    }
 
-    for (var i = 0; i < 5; i++) {
-        var playerInfo = JSON.parse(localStorage.getItem("player"));
-        console.log(playerInfo);
+    // Creates new li elements to contain the players' initials and scores and adds them to highscore table
+    for (var i = 0; i < playerTrack.length; i++) {        
+        var li = document.createElement("li");
+        li.textContent = playerTrack[i].initials + " - " + playerTrack[i].score;
+        highscoreList.appendChild(li);
+    }
+}
+
+// Clears the highscore list once clicked
+function clearList () {
+    localStorage.clear();
+    var childrenCount = highscoreList.children.length;
+
+    // Loops through li elements and removes them
+    for (var i = 0; i < childrenCount; i++) {
+        highscoreList.removeChild(highscoreList.children[0]);
     }
 }
